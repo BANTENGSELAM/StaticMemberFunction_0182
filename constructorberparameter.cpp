1d07c16b611e63b1a6ec33dbd9af7d6c51cadab8
“BANTENGSELAM” <pedjuangjava@gmail.com>
2026-05-04 15:53:59 +0700

    int main

--- constructorberparameter.cpp
+++ constructorberparameter.cpp
@@ -1,3 +1,20 @@
 #include <iostream>
 #include <string> // menyertakan library string
 using namespace std;
+
+class mahasiswa {
+    public:    
+            mahasiswa(int nim, string nama); // constructor dengan parameter
+};
+
+//Definisi Constructor dengan parameter
+mahasiswa::mahasiswa(int nim, string nama){
+        cout << "Constructor dengan Parameter terpanggil" << endl;
+        cout << "NIM : " << nim << endl;
+        cout << "Nama : " << nama << endl;
+}
+
+int main (){
+    mahasiswa mhs(12345, "Pascal"); // memanggil constructor dengan parameter
+    return 0;
+}
